4f657345cf46fef51c6430ad7bac915d83b23cb5
lain456 <iwakura.lain.pa@gmail.com>
2025-11-28 20:10:19 +0100

    yay fish works now thx guys :D

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php
 const C_FISH = '/../controller/FISH/fish.php';
 const FISH = "/../view/F/assets/fish.png";
+const FISH_CSS = "/../view/F/assets/fish.css";
 
-
-
-
-require_once __DIR__ . C_FISH;
-$controllerName = $_GET['c'] ?? 'fish';
-$action         = $_GET['a'] ?? 'index';
+require_once __DIR__ . C_FISH;                                                              
+$controllerName = $_GET['c'] ?? 'fish';                                                 
+$action         = $_GET['a'] ?? 'index';                                        
 
 switch ($controllerName) {
     case 'fish':
@@ -15,7 +13,7 @@ switch ($controllerName) {
         $controller = new fish();
         break;
 }
-if (!method_exists($controller, $action)) {
-    $action = 'index';
+if (!method_exists($controller, $action)) {                                                         
+    $action = 'index';                                                                  
 }
 $controller->$action();
